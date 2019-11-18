9b8a8c8f380909d8143f3dd7de871ad5820d416c
Mikita <kulbitskij.na@phystech.edu>
2019-11-18 21:03:37 +0300

    fixed problems

--- 03/library-parser.cpp
+++ 03/library-parser.cpp
@@ -1,23 +1,23 @@
 #include "library-parser.h"
 
-void MyNumberHandler(const std::string& data)
+void MyNumberHandler(const int& number)
 {
-	std::cout << "Received number = " << data << "\n";
+	std::cout << "Received number = " << number << "\n\n";
 }
 
 void MyStringHandler(const std::string& data)
 {
-	std::cout << "Received string: " << data << "\n";
+	std::cout << "Received string: " << data << "\n\n";
 }
 
 void StartHandler()
 {
-	std::cout << "Ready to parse tokens." << '\n';
+	std::cout << "Ready to parse tokens." << "\n\n";
 } 
 
 void EndHandler()
 {
-	std::cout << "Parsing completed." << '\n';
+	std::cout << "Parsing completed." << "\n\n";
 }
 
 int main()
--- 03/library-parser.h
+++ 03/library-parser.h
@@ -2,21 +2,31 @@
 #include <fstream>
 #include <string>
 
-using Handlers = void (*)(const std::string& data);
+
+using Number_handlers = void (*)(const int& number);
+using String_handlers = void (*)(const std::string& data);
 using Default_handlers = void (*)();
+
+bool isdigit(const char& symb)
+{
+	if((symb - '0' < 0) || (symb - '0' > 9))
+		return false;
+	else
+		return true;
+}
 
 class Handler
 {
 private:
-	int flag_;
+	bool flag_;
 	std::string command_;
-	Handlers NumberHandler_;
-	Handlers StringHandler_;
+	Number_handlers NumberHandler_;
+	String_handlers StringHandler_;
 	Default_handlers StartHandler_;
 	Default_handlers EndHandler_;
 
 public:
-	void regist(Handlers NumberHandler, Handlers StringHandler,
+	void regist(Number_handlers NumberHandler, String_handlers StringHandler,
 				Default_handlers StartHandler, Default_handlers EndHandler)
 	{
 		NumberHandler_ = NumberHandler;
@@ -25,13 +35,21 @@ public:
 		EndHandler_ = EndHandler;
 	}
 
-	void hand(const std::string& data, Handlers callback) 
+	void number_hand(const int& number, Number_handlers callback) 
 	{
+		std::cout << number << " is ready to be handled" << "\n";
+		callback(number);
+	}
+
+	void string_hand(const std::string& data, String_handlers callback) 
+	{
+		std::cout << data << " is ready to be handled" << "\n";
 		callback(data);
 	}
 
 	void default_hand(Default_handlers callback)
 	{
+		std::cout << "Default callback is ready to be called" << "\n";
 		callback();
 	}
 
@@ -39,22 +57,27 @@ public:
 	{
 		default_hand(StartHandler_);
 		std::ifstream in("input.txt");
+		int number = 0;
 		while(!in.eof())
 		{
-			flag_ = 0;
+			flag_ = true;
 			in >> command_;
 			for(int i = 0; i < command_.length(); i++)
 			{
-				if((command_[i] - '0' < 0) || (command_[i] - '0' > 9))
-					flag_++;
+				if(isdigit(command_[i]) == false)
+					flag_ = false;
 			}
-			if(flag_ == 0)
-				hand(command_, NumberHandler_);
+			if(flag_ == true)
+			{
+				number = atoi(command_.c_str());
+				number_hand(number, NumberHandler_);
+			}
 			else
-				hand(command_, StringHandler_);
+				string_hand(command_, StringHandler_);
 		}
 		in.close();
 		default_hand(EndHandler_);
 	}
 };
+
 
