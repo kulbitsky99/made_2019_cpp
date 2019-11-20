7500a7baa1d6b7356cc8f59a6bdffa12da5c2e14
Mikita <kulbitskij.na@phystech.edu>
2019-11-20 13:51:41 +0300

    structure changed

--- 06/BigInt.cpp
+++ 06/BigInt.cpp
@@ -1,9 +1,28 @@
 #include "BigInt.h"
 
+int BigInt::number_size(int number)
+{
+	int size = 0;
+	while(number)
+	{
+		number /= 10;
+		size++;
+	}
+	return size;
+}
+char* BigInt::alloc(int size)
+{
+	char* memory_ = (char*) calloc(size, sizeof(char));
+	assert(memory_);
+	return memory_;
+}
+
 void BigInt::swap(BigInt & rhs)
 {
 	std::swap(big_number_, rhs.big_number_);
 	std::swap(size_, rhs.size_);
+	//std::swap(sign_, rhs.sign_);
+
 }
 
 bool operator<(const BigInt& first_value, const BigInt& second_value)
@@ -154,15 +173,113 @@ BigInt& BigInt::operator+=(const BigInt& second_value)
 	return *this;	
 }
 
+/*BigInt& BigInt::operator-=(const BigInt& second_value)//takes unsigned numbers in my realization(ignore sign), but the answer has sign
+{
+	int symb = 0;
+	int mind = 0;
+	int result_size_ = 0;
+	int buffer_size = second_value.size_;
+	result_size_ = size_;
+	for(int i = 0; i < buffer_size; i++)
+	{
+		if(big_number_[i] < second_value.big_number_[i])
+		{
+			mind = 1;
+			symb = big_number_[i + 1] - '0' - 1;
+			big_number_[i + 1] = ((symb + 10) % 10) + '0';
+		}
+		else
+			mind = 0;
+		symb = 	big_number_[i] - '0' - 
+				second_value.big_number_[i] - '0' + mind * 10;
+		big_number_[i] = symb + '0';
+	}
+	
+	for(int i = buffer_size; i < size_; i++)
+		{
+			symb = big_number_[i] - '0' + mind;
+			big_number_[i] = (symb % 10) + '0';
+			mind = symb / 10;
+		}
+		if(mind == 1)
+		{
+			big_number_[size_] = 1 + '0';
+			result_size_++;
+		}
+
+	return *this;	
+}*/
+
+
+
 BigInt operator+(const BigInt& first_value, const BigInt& second_value)
 {
 	BigInt result = first_value;
 	result += second_value;
 	return result;
+	/*if((first_value.sign_ == true) && (second_value.sign_ == true))
+	{
+		BigInt result = first_value;
+		result += second_value;
+		result.sign_ = true;
+		return result;	
+	}
+	if((first_value.sign_ == false) && (second_value.sign_ == false))
+	{
+		BigInt result = first_value;
+		result += second_value;
+		result.sign_ = false;
+		return result;	
+	}
+	if((first_value.sign_ == false) && (second_value.sign_ == true))
+	{
+		BigInt result = second_value;
+		result -= first_value;
+		return result;	
+	}
+	if((first_value.sign_ == true) && (second_value.sign_ == false))
+	{
+		BigInt result = first_value;
+		result -= second_value;
+		return result;	
+	}*/
+
 }
+
+/*BigInt operator-(const BigInt& first_value, const BigInt& second_value)
+{
+	if((first_value.sign_ == true) && (second_value.sign_ == true))
+	{
+		BigInt result = first_value;
+		result -= second_value;
+		return result;	
+	}
+	if((first_value.sign_ == false) && (second_value.sign_ == false))
+	{
+		BigInt result = second_value;
+		result -= first_value;
+		return result;	
+	}
+	if((first_value.sign_ == true) && (second_value.sign_ == false))
+	{
+		BigInt result = first_value;
+		result += second_value;
+		result.sign_ = true;
+		return result;	
+	}
+	if((first_value.sign_ == false) && (second_value.sign_ == true))
+	{
+		BigInt result = first_value;
+		result += second_value;
+		result.sign_ = false;
+		return result;	
+	}
+}*/
 
 std::ostream & operator<<(std::ostream & stream, const BigInt & number)
 {
+		// if(sign_ == false)
+		// 	std::cout << "-";
         for(int i = number.size_ - 1; i >= 0; i--)
         {
                 std::cout << number.big_number_[i];
--- 06/BigInt.h
+++ 06/BigInt.h
@@ -4,28 +4,12 @@
 #include <cassert>
 #include <algorithm>
 
-int number_size(int number)
-{
-	int size = 0;
-	while(number)
-	{
-		number /= 10;
-		size++;
-	}
-	return size;
-}
-char* alloc(int size)
-{
-	char* memory_ = (char*) calloc(size, sizeof(char));
-	assert(memory_);
-	return memory_;
-}
-
 class BigInt
 {
 private:
 	char* big_number_;
 	size_t size_;
+	//bool sign_;
 public:
 	BigInt() 	: big_number_(nullptr)
 				, size_(0) 
@@ -38,14 +22,17 @@ public:
 			big_number_[i] = copied.big_number_[i];
 		}
 		size_ = copied.size_;
+		//sign_ = copied.sign_;
 	}
 	BigInt& operator=(const BigInt& copied)
 	{
+		big_number_ = (char*)realloc(big_number_, copied.size_ + 2);
 		for(int i = 0; i < copied.size_; i++)
 		{
 			big_number_[i] = copied.big_number_[i];
 		}
 		size_ = copied.size_;
+		//sign_ = copied.sign_;
 		return *this;
 	}
 	/*explicit*/BigInt(BigInt&& moved)
@@ -57,6 +44,26 @@ public:
 	{
 		BigInt::swap(moved);
 		return *this;
+	}
+
+	BigInt(const int& number)
+	{
+		int i = 0;
+		int symb = 0, num_cp = number;
+		big_number_ = alloc(number_size(num_cp) + 2);
+		while(num_cp != 0)
+		{
+			symb = num_cp % 10;
+			big_number_[i] = symb + '0';
+			num_cp /= 10;
+			i++;
+		}
+		size_ = i;
+		// if(num_cp >= 0)
+		// 	sign_ = true;
+		// else
+		// 	sign_ = false;
+
 	}
 
 	BigInt(int&& number)
@@ -72,16 +79,24 @@ public:
 			i++;
 		}
 		size_ = i;
+		// if(number >= 0)
+		// 	sign_ = true;
+		// else
+		// 	sign_ = false;
 	}
 	~BigInt() = default;
 
+	int number_size(int number);
+	char* alloc(int size);
 	void swap(BigInt & rhs);
 	BigInt& operator+=(const BigInt& second_value);
+	BigInt& operator-=(const BigInt& second_value);
 	friend bool operator<(const BigInt& first_value, const BigInt& second_value);
 	friend bool operator<=(const BigInt& first_value, const BigInt& second_value);
 	friend bool operator>(const BigInt& first_value, const BigInt& second_value);
 	friend bool operator>=(const BigInt& first_value, const BigInt& second_value);
 	friend BigInt operator+(const BigInt& first_value, const BigInt& second_value);
+	friend BigInt operator-(const BigInt& first_value, const BigInt& second_value);
 	friend  std::ostream & operator<<(std::ostream& stream, const BigInt& number);
 
 
